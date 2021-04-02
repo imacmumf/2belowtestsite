b37590af7903d04326c86e12f58f67d0cad5fed0
imacmumf <72942435+imacmumf@users.noreply.github.com>
2021-04-02 16:44:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -103,10 +103,10 @@ $(function () {
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
             $('.navbar').addClass('active');
-            $('.navIcon').attr("src", "images/icon_black.png")
+            $('.navIcon').attr("src", "Images/icon_black.png")
         } else {
             $('.navbar').removeClass('active');
-            $('.navIcon').attr("src", "images/icon1.png");
+            $('.navIcon').attr("src", "Images/icon1.png");
         }
     });
 });
